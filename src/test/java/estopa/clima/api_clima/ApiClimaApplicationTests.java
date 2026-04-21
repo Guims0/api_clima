2fe70ca409491d239b49759da4a96fc5cdd4f6fb
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-04-21 17:44:25 -0300

    iniciando novo projeto

---
+++ src/test/java/estopa/clima/api_clima/ApiClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package estopa.clima.api_clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
