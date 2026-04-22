abe77302b4161dc5ba58577ec474bb50a3792294
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-04-22 13:52:24 -0300

    atualizacao da classe entity

--- src/main/java/estopa/clima/api_clima/model/entity/Clima.java
+++ src/main/java/estopa/clima/api_clima/model/entity/Clima.java
@@ -2,14 +2,16 @@ package estopa.clima.api_clima.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_clima")
-@Data
+@Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Clima {
